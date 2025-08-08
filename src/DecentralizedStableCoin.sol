56706f353f220064e6f1527b7ae6b8026118ee5d
Nwachee <fuzzy245.in@gmail.com>
2025-08-08 15:16:29 +0100

    Fix forge fmt issue in workflow

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -26,7 +26,6 @@ pragma solidity ^0.8.19;
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-
 /* 
  * @title Decentralized Stable Coin
  * @author Nwachee
@@ -37,27 +36,29 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
  */
 
-contract DecentralizedStableCoin is ERC20Burnable, Ownable{
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DSC__BurnAmountMustBeGreaterThanZero();
     error DSC__BurnAmountExceedsBalance();
     error DSC__NotZeroAddress();
 
-    constructor() ERC20("Decentralized Stable Coin", "DSC"){}
+    constructor() ERC20("Decentralized Stable Coin", "DSC") {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
-        if(_amount <= 0) {
+        if (_amount <= 0) {
             revert DSC__BurnAmountMustBeGreaterThanZero();
-        } if (balance < _amount) {
+        }
+        if (balance < _amount) {
             revert DSC__BurnAmountExceedsBalance();
         }
         super.burn(_amount);
     }
 
     function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
-        if(_to == address(0)){
+        if (_to == address(0)) {
             revert DSC__NotZeroAddress();
-        } if (_amount <= 0) {
+        }
+        if (_amount <= 0) {
             revert DSC__BurnAmountMustBeGreaterThanZero();
         }
         _mint(_to, _amount);
