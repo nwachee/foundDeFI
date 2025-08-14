f51034c5cd247bccfecc6f212e8a7f865103a0f9
Nwachee <fuzzy245.in@gmail.com>
2025-08-14 23:11:59 +0100

    test: unit test for Collateral Revert

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -7,6 +7,9 @@ import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -15,6 +18,9 @@ contract DSCEngineTest is Test {
     HelperConfig config;
     address weth;
     address ethUsdPriceFeed;
+
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
 
     function setUp() public {
         deployer = new DeployDSC();
@@ -26,11 +32,24 @@ contract DSCEngineTest is Test {
     // Price Tests //
     /////////////////
 
-    function testGetUsdValue() public {
+    function test_GetUsdValue() public {
         // 15e18 * 2,000/ETH = 30,000e18
         uint256 ethAmount = 15e18;
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = dsce.getUsdValue(ethAmount, weth);
         assertEq(expectedUsd, actualUsd);
+    }
+
+    /////////////////
+    // deposit Tests //
+    /////////////////
+
+    function test_RevertIfCollateralIsZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__CollateralMustBeGreaterThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        
     }
 }
