d492bc3c5243fe720bd1e0d4202d812bed40b489
Nwachee <fuzzy245.in@gmail.com>
2025-08-30 23:54:58 +0100

    test(fuzz): add Handler test and integrate with InvariantsTest for invariant fuzzing

---
+++ test/fuzz/Invariants.t.sol
@@ -0,0 +1,43 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "./Handler.t.sol";
+
+contract OpenInvariantsTest is StdInvariant, Test {
+    DSCEngine dsce;
+    DeployDSC deployer;
+    DecentralizedStableCoin dsc;
+    HelperConfig helperConfig;
+    address weth;
+    address wbtc;
+    Handler handler;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, helperConfig) = deployer.run();
+        (,, weth, wbtc,) = helperConfig.activeNetworkConfig();
+        handler = new Handler(dsce, dsc);
+        targetContract(address(handler));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        // Get value of all collateral in the protocol
+        // Compare it to the total supply of all the Debt --> DSC
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalBtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 btcValue = dsce.getUsdValue(wbtc, totalBtcDeposited);
+
+        assert(wethValue + btcValue >= totalSupply);
+    }
+}
